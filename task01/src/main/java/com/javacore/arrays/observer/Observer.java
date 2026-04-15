278d3f56022513280e305a0bdbe9d314f21c621e
Marylin6 <marylin06@yandex.by>
2026-04-15 10:15:27 +0300

    rename

---
+++ task01/src/main/java/com/javacore/arrays/observer/Observer.java
@@ -0,0 +1,7 @@
+package main.java.com.javacore.arrays.observer;
+
+import main.java.com.javacore.arrays.entity.DoubleArrayEntity;
+
+public interface Observer {
+    void update(DoubleArrayEntity entity);
+}
---
+++ task01/src/main/java/com/javacore/arrays/specification/Specification.java
@@ -0,0 +1,7 @@
+package main.java.com.javacore.arrays.specification;
+
+import main.java.com.javacore.arrays.entity.DoubleArrayEntity;
+
+public interface Specification {
+    boolean specify(DoubleArrayEntity entity);
+}
