9908725117e8b3dca58eb5e2e78fae8bfc85d03f
Marylin6 <marylin06@yandex.by>
2026-04-17 22:05:03 +0300

    some changes in gitignore

--- arrays/src/main/java/com/javacore/arrays/entity/DoubleArrayEntity.java
+++ arrays/src/main/java/com/javacore/arrays/entity/DoubleArrayEntity.java
@@ -1,8 +1,9 @@
 package com.javacore.arrays.entity;
 
+import java.util.Arrays;
+
 import com.javacore.arrays.observer.Observer;
 
-import java.util.Arrays;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
